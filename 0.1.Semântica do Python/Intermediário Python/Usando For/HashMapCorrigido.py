8f461b4caf58f0212ceec0eafcc30c369ac6e283
Carlos <bearzotti.ce@gmail.com>
2025-10-17 16:33:22 -0300

    Ultimo commit antes da entrevista

--- 0.1.Semântica do Python/Intermediário Python/Usando For/HashMapCorrigido.py
+++ 0.1.Semântica do Python/Intermediário Python/Usando For/HashMapCorrigido.py
@@ -4,10 +4,10 @@ def countIncorrectPrices(products, productPrices, productSold, soldPrice):
         return 0
     
     if len(products) != len(productPrices):
-        raise ValueError("Products and productPrices must have the same length")
+        raise ValueError("Products e productPrices tem que ter o mesmo tamanho")
     
     if len(productSold) != len(soldPrice):
-        raise ValueError("productSold and soldPrice must have the same length")
+        raise ValueError("productSold e soldPrice tem que ter o mesmo tamanho")
     
     # Criar dicionário de preços corretos
     correct_prices = {}
@@ -41,37 +41,9 @@ def countIncorrectPrices(products, productPrices, productSold, soldPrice):
     
     return incorrect_count
 
-# Teste com o exemplo original
 products = ['ovos', 'leite', 'queijo', 'abacaxi']
 productPrices = [2.89, 3.29, 5.79, 3.33]
 productSold = ['ovos', 'ovos', 'queijo', 'leite']
 soldPrice = [2.89, 2.99, 5.97, 3.29]
 
-print(countIncorrectPrices(products, productPrices, productSold, soldPrice))  # Saída: 2
-
-# Testes adicionais com casos extremos
-print("\n=== Testes Adicionais ===")
-
-# Teste com valores nulos
-products2 = ['ovos', 'leite', None, 'queijo']
-productPrices2 = [2.89, 3.29, 5.79, None]
-productSold2 = ['ovos', None, 'queijo', 'leite']
-soldPrice2 = [2.89, 2.99, None, 3.29]
-
-print("Teste com nulos:", countIncorrectPrices(products2, productPrices2, productSold2, soldPrice2))
-
-# Teste com precisão decimal
-products3 = ['produto1', 'produto2']
-productPrices3 = [1.000000000001, 2.000000000000]
-productSold3 = ['produto1', 'produto2']
-soldPrice3 = [1.000000000002, 2.000000000000]  # Diferença na 12ª casa decimal
-
-print("Teste de precisão:", countIncorrectPrices(products3, productPrices3, productSold3, soldPrice3))
-
-# Teste com diferença mínima
-products4 = ['item']
-productPrices4 = [0.123456789012]
-productSold4 = ['item']
-soldPrice4 = [0.123456789013]  # Diferença de 0.000000000001
-
-print("Teste diferença mínima:", countIncorrectPrices(products4, productPrices4, productSold4, soldPrice4))
+print(countIncorrectPrices(products, productPrices, productSold, soldPrice))
